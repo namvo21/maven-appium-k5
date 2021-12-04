aaf53655044e4b4bc7b01d10f66ebc5cb4d43aef
namvo21 <fossilq2110@gmail.com>
2021-12-04 12:46:53 +0700

    Lesson 11

--- src/main/java/api_learning/LaunchingApp.java
+++ src/main/java/api_learning/LaunchingApp.java
@@ -1,4 +1,35 @@
 package api_learning;
 
+import caps.MobileCapabilityTypeEx;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
 public class LaunchingApp {
+    public static void main(String[] args) {
+
+        try {
+            AppiumDriver<MobileElement> appiumDriver = null;
+            // Specify capabilities
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.PLATFORM_NAME, "android");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.AUTOMATION_NAME, "uiautomator2");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.UDID, "ce081718bae29a2b047e");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_PACKAGE, "com.wdiodemoapp");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_ACTIVITY, "com.wdiodemoapp.MainActivity");
+
+            //Send the desireCaps into Appium Server
+            URL appiumServer = new URL("http://127.0.0.1:4723/wd/hub");
+            appiumDriver = new AndroidDriver<MobileElement>(appiumServer, desiredCapabilities);
+            appiumDriver.quit();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        // Init and create
+
+    }
 }
